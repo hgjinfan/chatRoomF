4fc4021857b572ea94f448f0eb89a79fc912f108
hgjf <xugaoxiang@easoncomm.com>
2018-10-08 18:27:14 +0800

    修改页面样式添加登陆按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <h1>chatRoom</h1>
-    <div>
+    <div class="login-form">
       <md-field>
-        <label>user name</label>
+        <md-icon>email</md-icon>
+        <label>User Name</label>
         <md-input v-model="formData.userName"></md-input>
       </md-field>
       <md-field>
-        <label>Password toggle</label>
+        <md-icon>lock</md-icon>
+        <label>Password</label>
         <md-input v-model="formData.passWord" type="password"></md-input>
       </md-field>
+      <md-button class="md-primary sign-in md-raised">sign in</md-button>
     </div>
   </div>
 </template>
@@ -24,7 +27,18 @@ export default {
         userName: "",
         passWord: "",
       }
-    };
+    }
   }
 };
 </script>
+
+<style scoped>
+  .login-form {
+    width: 6rem;
+    margin: 0 auto;
+  }
+  .sign-in {
+    width: 5rem;
+    
+  }
+</style>
